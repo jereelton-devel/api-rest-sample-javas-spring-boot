2fdc7a0f3df586d4fecb6152116e6672b60878c6
Jereelton Teixeira <jereeltondevel@gmail.com>
2022-01-28 11:58:20 -0300

    Initial Commit

---
+++ src/main/java/com/apirestsample/app/ApiRestSampleApplication.java
@@ -0,0 +1,13 @@
+package com.apirestsample.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apirestsample/app/ApiRestSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apirestsample.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
