668314b53419c1d0158132553be4da572f28c0f2
Jereelton Teixeira <jereeltondevel@gmail.com>
2022-01-30 22:31:17 -0300

    Creating routes to API

--- src/main/java/com/apirestsample/app/controllers/CustomerController.java
+++ src/main/java/com/apirestsample/app/controllers/CustomerController.java
@@ -3,14 +3,22 @@ package com.apirestsample.app.controllers;
 import com.apirestsample.app.entities.CustomerDTO;
 import com.apirestsample.app.entities.CustomerEntity;
 import com.apirestsample.app.services.CustomerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 @RestController
 @CrossOrigin(origins = "*")
 @Tag(name = "Rest Api Sample")
+@Schema(implementation = CustomerDTO.class)
 public class CustomerController {
 
     private final CustomerService customerService;
@@ -20,37 +28,115 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
-    /**
-     * Create User
-     */
+    @Operation(summary = "Add new customer")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Customer created successfully", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = CustomerDTO.class))
+            }),
+            @ApiResponse(responseCode = "302", description = "Customer name already exists", content = {
+                    @Content(mediaType = "text")
+            }),
+            @ApiResponse(responseCode = "400", description = "Missing body request", content = {
+                    @Content(mediaType = "text")
+            }),
+            @ApiResponse(responseCode = "500", description = "Server error", content = {
+                    @Content(mediaType = "text")
+            })
+    })
     @PostMapping(path = "/api/customers")
-    public ResponseEntity<CustomerDTO> save(@RequestBody CustomerEntity customer) {
+    public ResponseEntity<?> create(@Valid @RequestBody(required = false) CustomerEntity customer) {
         return customerService.save(customer);
     }
 
-    /**
-     * Get User
-     */
+    @Operation(summary = "Get all customers")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK, read all customers", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = CustomerDTO.class))
+            }),
+            @ApiResponse(responseCode = "404", description = "Customers not found", content = {
+                    @Content(mediaType = "text")
+            }),
+            @ApiResponse(responseCode = "500", description = "Server error", content = {
+                    @Content(mediaType = "text")
+            })
+    })
     @GetMapping(path = "/api/customers")
-    public ResponseEntity<?> findAll() {
+    public ResponseEntity<?> readAll() {
         return customerService.findAll();
     }
 
-    /**
-     * Get Users: id
-     */
+    @Operation(summary = "Get customer by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK, read one customer", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = CustomerDTO.class))
+            }),
+            @ApiResponse(responseCode = "404", description = "Customer not found", content = {
+                    @Content(mediaType = "text")
+            }),
+            @ApiResponse(responseCode = "500", description = "Server error", content = {
+                    @Content(mediaType = "text")
+            })
+    })
     @GetMapping(path = "/api/customers/{customer_id}")
-    public ResponseEntity<CustomerDTO> findId(@PathVariable("customer_id") String customer_id) {
+    public ResponseEntity<?> read(@PathVariable("customer_id") String customer_id) {
         return customerService.findById(customer_id);
-
     }
 
-    /**
-     * Update User: id
-     */
+    @Operation(summary = "Update an customer")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK, Customer updated", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = CustomerDTO.class))
+            }),
+            @ApiResponse(responseCode = "400", description = "Missing body request", content = {
+                    @Content(mediaType = "text")
+            }),
+            @ApiResponse(responseCode = "404", description = "Customer not found", content = {
+                    @Content(mediaType = "text")
+            }),
+            @ApiResponse(responseCode = "500", description = "Server error", content = {
+                    @Content(mediaType = "text")
+            })
+    })
+    @PutMapping (value = "/api/customers/{customer_id}")
+    public ResponseEntity<?> update(@PathVariable("customer_id") String customer_id, @RequestBody CustomerEntity customer_data) {
+        return customerService.updateCustomer(customer_id, customer_data);
+    }
 
-    /**
-     * Delete User:id
-     */
+    @Operation(summary = "Delete an customer by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK, customer deleted", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = CustomerDTO.class))
+            }),
+            @ApiResponse(responseCode = "404", description = "Customer not found", content = {
+                    @Content(mediaType = "text")
+            }),
+            @ApiResponse(responseCode = "500", description = "Server error", content = {
+                    @Content(mediaType = "text")
+            })
+    })
+    @DeleteMapping (path = "/api/customers/{customer_id}")
+    public ResponseEntity<?> delete(@PathVariable("customer_id") String customer_id) {
+        return customerService.deleteCustomer(customer_id);
+    }
+
+    @Operation(summary = "Fix/Patch an customer")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK, Customer patched", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = CustomerDTO.class))
+            }),
+            @ApiResponse(responseCode = "400", description = "Missing body request", content = {
+                    @Content(mediaType = "text")
+            }),
+            @ApiResponse(responseCode = "404", description = "Customer not found", content = {
+                    @Content(mediaType = "text")
+            }),
+            @ApiResponse(responseCode = "500", description = "Server error", content = {
+                    @Content(mediaType = "text")
+            })
+    })
+    @PatchMapping (path = "/api/customers/{customer_id}")
+    public ResponseEntity<?> fix(@PathVariable("customer_id") String customer_id, @RequestBody CustomerEntity customer_data) {
+        return customerService.patchCustomer(customer_id, customer_data);
+    }
 
 }
--- src/main/java/com/apirestsample/app/services/CustomerService.java
+++ src/main/java/com/apirestsample/app/services/CustomerService.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CustomerService {
@@ -21,20 +22,125 @@ public class CustomerService {
         this.customerRepository = customerRepository;
     }
 
-    public ResponseEntity<CustomerDTO> save(CustomerEntity customer) {
-        CustomerEntity customerSave = customerRepository.save(customer);
-        return ResponseEntity.status(HttpStatus.CREATED).body(CustomerDTO.mapperCustomerDTO(customerSave));
+    public CustomerEntity findUserByName(String username) {
+        return customerRepository.findByUsername(username);
+    }
+
+    public ResponseEntity<?> save(CustomerEntity customer) {
+
+        if (customer == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Missing body request");
+        }
+
+        if (findUserByName(customer.getName()) != null) {
+            return ResponseEntity.status(HttpStatus.FOUND)
+                .body("Customer ["+customer.getName()+"] already exists");
+        }
+
+        try {
+            CustomerEntity customerSave = customerRepository.save(customer);
+            return ResponseEntity.status(HttpStatus.CREATED).body(CustomerDTO.mapperCustomerDTO(customerSave));
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Occurs an error not expected in the server");
+        }
     }
 
     public ResponseEntity<?> findAll() {
-        List<CustomerEntity> listAll = customerRepository.findAll();
-        JSONObject customersDTO = CustomerDTO.mapperAllCustomerDTO(listAll);
-        return ResponseEntity.status(HttpStatus.OK).body(customersDTO);
+        try {
+            List<CustomerEntity> listAll = customerRepository.findAll();
+            JSONObject customersDTO = CustomerDTO.mapperAllCustomerDTO(listAll);
+            if (customersDTO.size() > 0) {
+                return ResponseEntity.status(HttpStatus.OK).body(customersDTO);
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Customers not found");
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Occurs an error not expected in the server");
+        }
     }
 
-    public ResponseEntity<CustomerDTO> findById(String customer_id) {
-        return customerRepository.findById(customer_id)
-            .map(record -> ResponseEntity.ok().body(CustomerDTO.mapperCustomerDTO(record)))
-            .orElse(ResponseEntity.notFound().build());
+    public ResponseEntity<?> findById(String customer_id) {
+        try {
+            Optional<CustomerEntity> customer = customerRepository.findById(customer_id);
+
+            if (customer.stream().findAny().isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Customer not found");
+            } else {
+                return customerRepository.findById(customer_id)
+                    .map(record -> ResponseEntity.ok().body(CustomerDTO.mapperCustomerDTO(record)))
+                    .orElse(ResponseEntity.notFound().build());
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Occurs an error not expected in the server");
+        }
+    }
+
+    public ResponseEntity<?> updateCustomer(String customer_id, CustomerEntity customer_data) {
+
+        if (customer_data == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Missing body request");
+        }
+
+        try {
+            Optional<CustomerEntity> customer = customerRepository.findById(customer_id);
+
+            if (customer.stream().findAny().isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Customer not found");
+            } else {
+                return customerRepository.findById(customer_id)
+                    .map( record -> {
+                        record.setName(customer_data.getName());
+                        record.setActive(customer_data.getActive());
+                        CustomerEntity updated = customerRepository.save(record);
+                        return ResponseEntity.ok().body(CustomerDTO.mapperCustomerDTO(updated));
+                    }).orElse(ResponseEntity.notFound().build());
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Occurs an error not expected in the server");
+        }
+    }
+
+    public ResponseEntity<?> deleteCustomer(String customer_id) {
+        try {
+            Optional<CustomerEntity> customer = customerRepository.findById(customer_id);
+
+            if (customer.stream().findAny().isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Customer not found");
+            } else {
+                return customerRepository.findById(customer_id)
+                        .map(record -> {
+                            customerRepository.deleteById(customer_id);
+                            return ResponseEntity.ok().body(CustomerDTO.mapperCustomerDTO(record));
+                        }).orElse(ResponseEntity.notFound().build());
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Occurs an error not expected in the server");
+        }
+    }
+
+    public ResponseEntity<?> patchCustomer(String customer_id, CustomerEntity customer_patch) {
+
+        if (customer_patch == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Missing body request");
+        }
+
+        try {
+            Optional<CustomerEntity> customer = customerRepository.findById(customer_id);
+
+            if (customer.stream().findAny().isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Customer not found");
+            } else {
+                return customerRepository.findById(customer_id)
+                        .map( record -> {
+                            if (customer_patch.getName() != null) record.setName(customer_patch.getName());
+                            if (customer_patch.getActive() != null) record.setActive(customer_patch.getActive());
+                            CustomerEntity patcher = customerRepository.save(record);
+                            return ResponseEntity.ok().body(CustomerDTO.mapperCustomerDTO(patcher));
+                        }).orElse(ResponseEntity.notFound().build());
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Occurs an error not expected in the server");
+        }
     }
 }
