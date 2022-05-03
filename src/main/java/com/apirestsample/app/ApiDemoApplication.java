66238eda0d8e0bbbed2dc4f88b6193855799c05b
Jereelton Teixeira <jereeltondevel@gmail.com>
2022-05-03 14:25:39 -0300

    Branch especifica

--- src/main/java/com/apirestsample/app/ApiDemoApplication.java
+++ src/main/java/com/apirestsample/app/ApiDemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiRestSampleApplication {
+public class ApiDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiRestSampleApplication.class, args);
+		SpringApplication.run(ApiDemoApplication.class, args);
 	}
 
 }
